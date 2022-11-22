d21459ddaeedbc6a9751263d311419d8d74867fc
jy-na <njy305@hkcloud.co.kr>
2022-11-22 11:55:54 +0900

    백준 4번

---
+++ searchMax.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class searchMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        int[] createArr = new int[arr.length];
+        int max = 0;
+        int count=0;
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = (int)(Math.random()*100);
+            createArr = arr;
+            System.out.println("arr["+i+"] : "+arr[i]);
+            if(arr[0]<arr[i]){
+                arr[0] = arr[i];
+                max = arr[i];
+            }
+            if(max == createArr[i]){
+                count = i;
+            }
+
+        }
+
+            System.out.println("최댓값 : "+max+"\n배열의 인덱스 : " + count+"\n자릿수 : "+ (count+1));
+
+    }
+}
